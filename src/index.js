ee0728dda8fb8d36635256db46a587774021c957
byzz <eyedroot@gmail.com>
2018-06-05 21:24:05 +0900

    modify: Store exmaples

--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ import App from './components/App'
 
 import { createStore } from 'redux'
 import reducers from './reducers'
-// import * as actions from './actions'
+// import * as ACTIONS from './actions'
 
 const store = createStore(reducers)
 
-// console.log(store.getState())
-// const unsubscribe = store.subscribe(() => console.log(store.getState()))
-// store.dispatch(actions.increment())
-// store.dispatch(actions.increment())
-// store.dispatch(actions.decrement())
-// store.dispatch(actions.setColor([200, 200, 200]))
+/**
+ * Store의 역할
+ * 
+ * 1. dispatch(action)
+ * 2. getState()
+ * 3. subscribe(listener)
+ * 4. replaceReducer(nextReducer)
+ */
+// const unsubscribe = store.subscribe(() => console.log( store.getState() ))
+
+// store.dispatch( ACTIONS.increment() )
+// store.dispatch( ACTIONS.increment() )
+// store.dispatch( ACTIONS.decrement() )
+// store.dispatch( ACTIONS.setColor([200, 200, 200]) )
 
 // unsubscribe()
 
-// store.dispatch(actions.setColor([210, 210, 210]))
+// store.dispatch( ACTIONS.setColor([111, 111, 111]) )
 
 ReactDOM.render(<App />, document.getElementById('root'))
